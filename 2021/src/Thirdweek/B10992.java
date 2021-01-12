37a67de1bd1b022822516c84119c6f11a6485074
kim <tjdwns6481@naver.com>
2021-01-12 23:51:30 +0900

    "1/13"

--- 2021/src/Thirdweek/B10992.java
+++ 2021/src/Thirdweek/B10992.java
@@ -8,6 +8,7 @@ public class B10992 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
 
         for(int i=0; i<N-1; i++ ){
