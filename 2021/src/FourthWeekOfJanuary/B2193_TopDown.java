663c3f69dc9ca5fc1dbf1c4160d33242c7c64994
kim <tjdwns6481@naver.com>
2021-01-30 00:26:30 +0900

    "1/30"

---
+++ 2021/src/FourthWeekOfJanuary/B2193_TopDown.java
@@ -0,0 +1,25 @@
+package FourthWeekOfJanuary;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2193_TopDown {
+    static long [] dp = null;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        dp = new long[N+1];
+        System.out.println(TopDown(N));
+    }
+
+    public static long TopDown(int dp_index){
+        if(dp_index==1 || dp_index==2 ){
+            return 1;
+        }if(dp[dp_index]>0){
+            return dp[dp_index];
+        } else{
+            return dp[dp_index] = TopDown(dp_index-1)+TopDown(dp_index-2);
+        }
+    }
+}
