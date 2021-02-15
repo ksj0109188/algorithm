f290eb3ec75458a03bc5c4eb5408a96872e1bb72
kim <tjdwns6481@naver.com>
2021-02-16 00:54:24 +0900

    "2/16"

---
+++ 2021/src/February_2021/ThirdWeek/B1037.java
@@ -0,0 +1,25 @@
+package February_2021.ThirdWeek;
+
+import java.util.Scanner;
+//약수
+//https://www.acmicpc.net/problem/1037
+public class B1037 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int [] A = new int[N];
+        int min = 1000001;
+        int max = 0;
+        for (int i=0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+        for(int i=0; i<N; i++){
+            if(A[i]<min){
+                min = A[i];
+            }if(A[i]>max){
+                max = A[i];
+            }
+        }
+        System.out.println(min*max);
+    }
+}
---
+++ 2021/src/February_2021/ThirdWeek/B4375.java
@@ -0,0 +1,22 @@
+package February_2021.ThirdWeek;
+
+import java.util.Scanner;
+
+public class B4375 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int N = scanner.nextInt();
+            int Num = 0;
+            for (int i = 1; ; i++) {
+                Num = Num * 10 + 1;
+                Num %= N;
+                if(Num==0){
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+        scanner.close();
+    }
+}
