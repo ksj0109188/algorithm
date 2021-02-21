113ff5170793d2c823a27f34d0b9382ea2d2cd04
kim <tjdwns6481@naver.com>
2021-02-22 00:41:24 +0900

    "2/20"

---
+++ 2021/src/February_2021/ThirdWeek/B1929.java
@@ -0,0 +1,33 @@
+package February_2021.ThirdWeek;
+
+import java.util.Scanner;
+// https://www.acmicpc.net/problem/1929
+// 1929
+public class B1929 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int M = scanner.nextInt();
+        int N = scanner.nextInt();
+        boolean[] result = (check_prime(M, N));
+        for (int i = M; i <= N; i++) {
+            if (result[i] == false) {
+                System.out.println(i);
+            }
+        }
+        scanner.close();
+    }
+
+    private static boolean[] check_prime(int m, int n) {
+        boolean[] primeNumbers = new boolean[n + 1];
+        primeNumbers[0] = primeNumbers[1] = true; // 지워짐
+        for (int i = 2; i * i <= n; i++) {
+            if (primeNumbers[i] == true) {
+                continue;
+            }
+            for (int j = i + i; j <= n; j += i) {
+                primeNumbers[j] = true;
+            }
+        }
+        return primeNumbers;
+    }
+}
---
+++ 2021/src/February_2021/ThirdWeek/B1978.java
@@ -0,0 +1,34 @@
+package February_2021.ThirdWeek;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class B1978 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            int num = scanner.nextInt();
+            if (check_primeNumber(num)) {
+                result += 1;
+            }
+        }
+        System.out.println(result);
+        scanner.close();
+    }
+
+    private static boolean check_primeNumber(int num) {
+        if (num < 2) {
+            return false;
+        } else if (num == 2) {
+            return true;
+        }
+        for (int j = 2; j * j <= num; j++) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
