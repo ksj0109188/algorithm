1243a151c72cdd5326e33eb3ea8b1c3315894c86
Kim Seong Jun <48472569+ksj0109188@users.noreply.github.com>
2024-10-25 10:43:39 +0900

    10/25일자 알고리즘

--- 2024/sources/main.swift
+++ 2024/sources/main.swift
@@ -626,4 +626,30 @@ let arr2 = [
 //N과M12_15666().solution()
 
 // 10/24
-RGB1149().solution()
+//RGB1149().solution()
+곱셈1629().solution()
+
+class 곱셈1629 {
+    func modularExponentiation(_ A: Int, _ B: Int, _ C: Int) -> Int {
+        if B == 0 {
+            return 1
+        }
+        
+        let half = modularExponentiation(A, B / 2, C)
+        
+        if B % 2 == 0 {
+            return  ( half * half ) % C
+        } else {
+            return (half * half * A) % C
+        }
+    }
+
+    func solution() {
+        let input = readLine()!.split(separator: " ").map{ Int($0)! }
+        let A = input[0]
+        let B = input[1]
+        let C = input[2]
+        
+        print(modularExponentiation(A, B, C))
+    }
+}
