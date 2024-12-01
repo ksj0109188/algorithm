17c95ab4d16d43841f118186d59966928d639d94
Seongjun Kim <ksj0109188@gmail.com>
2024-12-01 19:26:27 +0900

    알고리즘 문제풀이

--- 2024/sources/BakJoon/BackTracking/NQueen.swift
+++ 2024/sources/BakJoon/BackTracking/NQueen.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+
+class NQueen_9663 {
+    var result = 0
+      
+      func solution() {
+          let n = Int(readLine()!)!
+          dfs(queens: [], count: 0, n: n)
+          print(result)
+      }
+      
+      private func dfs(queens: [(i: Int, j: Int)], count: Int, n: Int) {
+          if count == n {
+              result += 1
+              return
+          }
+          
+          for j in 0..<n {
+              if isPossible(i: count, j: j, queens: queens) {
+                  dfs(queens: queens + [(i: count, j: j)], count: count + 1, n: n)
+              }
+          }
+      }
+      
+      private func isPossible(i: Int, j: Int, queens: [(i: Int, j: Int)]) -> Bool {
+          for queen in queens {
+              if queen.i == i || queen.j == j { // 수직, 수평 체크
+                  return false
+              }
+              if abs(queen.i - i) == abs(queen.j - j) { // 대각선 체크
+                  return false
+              }
+          }
+          return true
+      }
+}
--- 2024/sources/BakJoon/Tree/1967.swift
+++ 2024/sources/BakJoon/Tree/1967.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class 트리의지름_1967 {
+    func solution() {
+        let n = Int(readLine()!)!
+        var graph: [Int: [(child: Int, weight:Int)]] = [:]
+        var answer: Int = 0
+        
+        for _ in 0..<n - 1{
+            let input = readLine()!.split(separator: " ").map{ Int($0)! }
+            let parent = input[0]
+            let child = input[1]
+            let weight = input[2]
+            
+            graph[parent, default: []].append((child: child, weight: weight))
+        }
+        
+        dfs(parent: 1, currentWeight: 0)
+
+        print(answer)
+        
+        func dfs(parent: Int, currentWeight: Int) -> Int {
+            guard let nextNodes = graph[parent] else { return currentWeight }
+            
+            var weights: [Int] = []
+            
+            for next in nextNodes {
+                weights += [dfs(parent: next.child, currentWeight: next.weight)]
+            }
+            
+            answer = max(weights.reduce(0) { $0 + $1 }, answer) // 자식들 노드의 누적된 경로를 비교
+            
+            return (weights.max() ?? 0) + currentWeight // 누적된 자식들중 가장 큰값과 현재 경로의 가중치를 더해 return
+        }
+    }
+}
