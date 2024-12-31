f3f40410b45e6979b69d4361eaf79ffbaf2da39d
Seongjun Kim <ksj0109188@gmail.com>
2024-12-31 12:23:48 +0900

    투 포인터 문제 풀이

--- 2024/sources/main.swift
+++ 2024/sources/main.swift
@@ -727,26 +727,21 @@ class 부분합_1806{
         let n = input[0]
         let s = input[1]
         let arr = readLine()!.split(separator: " ").map{ Int($0)! }
-        var count = 1
-        var result = Int.max
+        var start = 0
+        var end = 0
         var sum = 0
-        var i = 0
+        var result = Int.max
         
-        while i < arr.count {
-            if result == 1 {
-                break
+        while end < n {
+            sum += arr[end]
+            
+            while sum >= s {
+                result = min(result, end - start + 1)
+                sum -= arr[start]
+                start += 1
             }
             
-            sum += arr[i]
-            
-            if sum >= s {
-                result = min(result, count)
-                sum = 0
-                count = 1
-            } else {
-                count += 1
-                i += 1
-            }
+            end += 1
         }
         
         if result == Int.max {
