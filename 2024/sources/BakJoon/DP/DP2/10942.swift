5269b9ef10dadfdb31f86ff1ddd66f45546601ec
Seongjun Kim <ksj0109188@gmail.com>
2025-01-12 18:02:39 +0900

    팰린드롬 수정

--- 2024/sources/BakJoon/DP/DP2/10942.swift
+++ 2024/sources/BakJoon/DP/DP2/10942.swift
@@ -7,59 +7,67 @@
 
 import Foundation
 
-
-
-class 팰린드롬 {
-    final class FileIO {
-        private var buffer:[UInt8]
-        private var index: Int
-        
-        init(fileHandle: FileHandle = FileHandle.standardInput) {
-            buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-            index = 0
-        }
-        
-        @inline(__always) private func read() -> UInt8 {
-            defer { index += 1 }
-            
-            return buffer.withUnsafeBufferPointer { $0[index] }
-        }
-        
-        @inline(__always) func readInt() -> Int {
-            var sum = 0
-            var now = read()
-            var isPositive = true
-            
-            while now == 10
-                    || now == 32 { now = read() } // 공백과 줄바꿈 무시
-            if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
-            while now >= 48, now <= 57 {
-                sum = sum * 10 + Int(now-48)
-                now = read()
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        do {
+            guard let fileData = try fileHandle.readToEnd() else {
+                debugPrint("Empty Data")
+                exit(0)
             }
-            
-            return sum * (isPositive ? 1:-1)
+            buffer = Array(fileData) + [UInt8(0)] // 인덱스 범위 초과 방지
+        } catch {
+            debugPrint("Error reading data: \(error.localizedDescription)")
+            exit(0)
         }
-        
-        @inline(__always) func readString() -> String {
-            var str = ""
-            var now = read()
-            
-            while now == 10
-                    || now == 32 { now = read() } // 공백과 줄바꿈 무시
-            
-            while now != 10
-                    && now != 32 && now != 0 {
-                str += String(bytes: [now], encoding: .ascii)!
-                now = read()
-            }
-            
-            return str
-        }
+        index = 0
     }
     
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+class 팰린드롬 {
+    let file = FileIO()
+    
     func solution() {
-        let file = FileIO()
         let n = file.readInt()
         var nums: [Int] = []
         
