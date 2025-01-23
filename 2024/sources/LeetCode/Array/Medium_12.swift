e88fac4982b4bd3dc07af8df23ceffcffaa97c1f
Seongjun Kim <ksj0109188@gmail.com>
2025-01-23 12:45:42 +0900

    배열 구현 문제풀이

---
+++ 2024/sources/LeetCode/Array/Medium_12.swift
@@ -0,0 +1,42 @@
+//
+//  Medium_12.swift
+//  BakJoon
+//
+//  Created by 김성준 on 1/23/25.
+//
+
+import Foundation
+
+class Medium_12 {
+func intToRoman(_ num: Int) -> String {
+    // Roman numeral symbols and their corresponding values
+    let romanSymbols = [
+        (value: 1000, symbol: "M"),
+        (value: 900, symbol: "CM"),
+        (value: 500, symbol: "D"),
+        (value: 400, symbol: "CD"),
+        (value: 100, symbol: "C"),
+        (value: 90, symbol: "XC"),
+        (value: 50, symbol: "L"),
+        (value: 40, symbol: "XL"),
+        (value: 10, symbol: "X"),
+        (value: 9, symbol: "IX"),
+        (value: 5, symbol: "V"),
+        (value: 4, symbol: "IV"),
+        (value: 1, symbol: "I")
+    ]
+    
+    var result = ""
+    var number = num
+    
+    // Iterate through the Roman numeral values
+    for roman in romanSymbols {
+        while number >= roman.value {
+            result += roman.symbol
+            number -= roman.value
+        }
+    }
+    
+    return result
+}
+}
