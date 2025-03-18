b6b744a703d7fd06020089baa92d412de9cdff41
Seongjun Kim <ksj0109188@gmail.com>
2025-03-18 13:58:44 +0900

    LinkedList Hard문제 재시도

--- 2024/sources/LeetCode/LinkedList/Hard_25.swift
+++ 2024/sources/LeetCode/LinkedList/Hard_25.swift
@@ -7,34 +7,54 @@
 
 import Foundation
 
-class Hard_25 {
-    func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
-        var count = 1
-        var dummy = head
-        var start = head
-
-        while dummy != nil {
-            if count == k {
-//                reverse(start, dummy!)
-//                start = dummy.next
-            }
-
-            dummy = dummy?.next
-        }
-
-        return dnummy
-
-    }
-
-    private func reverse(_ prevStart: ListNode, _ k: Int) -> ListNode {
-        var start = prevStart.next
-        var then = start?.next
-
-        for _ in 0..<k {
-            start?.next = then?.next
-            then?.next =
-            prevStart = then
-            then = start?.next
-        }
-    }
-}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+//class Hard_25 {
+//    func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
+//        var count = 1
+//        var dummy: ListNode? = ListNode(0)
+//        dummy?.next = head
+//        var prev: ListNode? = dummy
+//
+//        while dummy != nil {
+//            if count % k == 0{
+//                print("count", count)
+//                print(prev?.val)
+//                reverse(prev, k)
+//                prev = dummy
+//            }
+//
+//            count += 1
+//            dummy = dummy?.next
+//        }
+//
+//        return head
+//    }
+//
+//    private func reverse(_ prevStart: ListNode?, _ k: Int) {
+//        var start = prevStart?.next
+//        var then = start?.next
+//
+//        for _ in 0..<k {
+//            start?.next = then?.next
+//            then?.next = prevStart?.next
+//            prevStart?.next = then
+//            then = start?.next
+//        }
+//
+//        var dummy = prevStart
+//
+//        while dummy != nil {
+//            print("revsered item", dummy?.val)
+//            dummy = dummy?.next
+//        }
+//    }
+//}
