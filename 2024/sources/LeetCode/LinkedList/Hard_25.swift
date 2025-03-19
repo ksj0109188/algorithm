056f01377777144e690e7519c95628d657c0fb84
Seongjun Kim <ksj0109188@gmail.com>
2025-03-19 11:58:48 +0900

    Linked List Hard문제 풀이

--- 2024/sources/LeetCode/LinkedList/Hard_25.swift
+++ 2024/sources/LeetCode/LinkedList/Hard_25.swift
@@ -17,44 +17,39 @@ import Foundation
  *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
  * }
  */
-//class Hard_25 {
-//    func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
-//        var count = 1
-//        var dummy: ListNode? = ListNode(0)
-//        dummy?.next = head
-//        var prev: ListNode? = dummy
-//
-//        while dummy != nil {
-//            if count % k == 0{
-//                print("count", count)
-//                print(prev?.val)
-//                reverse(prev, k)
-//                prev = dummy
-//            }
-//
-//            count += 1
-//            dummy = dummy?.next
-//        }
-//
-//        return head
-//    }
-//
-//    private func reverse(_ prevStart: ListNode?, _ k: Int) {
-//        var start = prevStart?.next
-//        var then = start?.next
-//
-//        for _ in 0..<k {
-//            start?.next = then?.next
-//            then?.next = prevStart?.next
-//            prevStart?.next = then
-//            then = start?.next
-//        }
-//
-//        var dummy = prevStart
-//
-//        while dummy != nil {
-//            print("revsered item", dummy?.val)
-//            dummy = dummy?.next
-//        }
-//    }
-//}
+class Hard_25 {
+    func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
+        var count = 1
+        var dummy: ListNode? = ListNode(0)
+        dummy?.next = head
+        var result = dummy
+        var prev: ListNode? = dummy
+        
+        while dummy != nil {
+            if count % k == 0 {
+                prev = reverse(prev, k)
+                dummy = prev?.next
+            } else {
+                dummy = dummy?.next
+            }
+            
+            count += 1
+        }
+        
+        return result?.next
+    }
+    
+    private func reverse(_ prevStart: ListNode?, _ k: Int) -> ListNode? {
+        var start = prevStart?.next
+        var then = start?.next
+        
+        for _ in 1..<k {
+            start?.next = then?.next
+            then?.next = prevStart?.next
+            prevStart?.next = then
+            then = start?.next
+        }
+        
+        return start
+    }
+}
